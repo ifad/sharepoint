562e2ad62fd85a21afd6136b15cefbf5d36cb18d
Davide Papagni <davide@algonauti.com>
2016-07-22 14:21:30 +0200

    helpers to mock requests and responses

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,4 +1,5 @@
 module Helpers
+
   def value_to_string(value)
     case value
       when nil
@@ -9,6 +10,24 @@ module Helpers
         value
     end
   end
+
+  def mock_requests
+    allow_any_instance_of(Ethon::Easy)
+      .to receive(:perform)
+      .and_return(nil)
+  end
+
+  def mock_responses(fixture_file)
+    allow_any_instance_of(Ethon::Easy)
+      .to receive(:response_code)
+      .and_return(200)
+    allow_any_instance_of(Ethon::Easy)
+      .to receive(:response_body)
+      .and_return(
+        File.open("spec/fixtures/responses/#{fixture_file}").read
+      )
+  end
+
 end
 
 RSpec.configure do |config|

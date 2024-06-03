ce361af13b7ee3278b4964567c5b5720de4c1b9e
Danilo Grieco <grieco.danilo@gmail.com>
2024-06-03 10:50:10 +0200

    Bump version v0.2.0

---
+++ lib/sharepoint/version.rb
@@ -0,0 +1,12 @@
+module Sharepoint
+  module Version
+
+    MAJOR = 0
+    MINOR = 2
+    PATCH = 0
+    BUILD = nil
+
+    VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join(".").freeze
+  end
+
+end

a96a491c0d5c08c928158140ce4ce2524505b00e
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-02-21 14:56:08 +0100

    Enable frozen string literal in production code
    At the best of my knowledge, this should not break.
    
    In any case, bumping the minor number
    
    Additionally, make the version available in production code

--- lib/sharepoint.rb
+++ lib/sharepoint.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'sharepoint/errors'
 require_relative 'sharepoint/client'
+require_relative 'sharepoint/version'
--- lib/sharepoint/version.rb
+++ lib/sharepoint/version.rb
@@ -3,8 +3,8 @@
 module Sharepoint
   module Version
     MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
 
     VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').freeze
